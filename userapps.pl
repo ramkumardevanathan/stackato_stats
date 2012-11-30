03cb2197528e01f7ef0b2ddf64b300c00955b4e6
Ramkumar Devanathan <ramd@hp.com>
2012-11-30 11:39:25 -0800

    some more fixes.

--- userapps.pl
+++ userapps.pl
@@ -7,24 +7,33 @@ use fns;
 
 my $users = fns::decode("stackato users --json");
 
-print "EPOCH\tappinstance\tuser\turi\tquota\tused\n";
-
 foreach my $userhash (@$users) {
 	my $epoch = fns::time_msecs;
 	my $useremail = $userhash->{'email'};
-	my $admin_role = $userhash->{'admin'};
 	my $appsarray = $userhash->{'apps'};
-	my $groups = $userhash->{'groups'};
-	$groups =~ tr/ /,/;
 
-	my $appcount = scalar (@$appsarray);
+	foreach my $apphash (@$appsarray) {
+		my $app = $apphash->{"name"};
+		fns::login ($useremail);
+		open (ASPL, "perl appstats.pl $app|") or die ("unable to run appstats.pl: $!");
 
-	my $mem_info_hash = fns::decode("stackato usage $useremail --json");
-	my $allocated_mb = $mem_info_hash->{'allocated'}->{'mem'}/1024;
-	my $usage_mb = $mem_info_hash->{'usage'}->{'mem'}/1024;
+		my $firstline="";
+		while (<ASPL>) {
+		  my $line = $_;
+		  if ($firstline eq "") {
+		    $firstline = $_;
+		    next;
+		  }
+		  my @parts = split (/\t/, $line);
+		  my $busfilename = 
+			"APP_INSTANCE\$!".$useremail.".".$parts[1].".bus";
+		  open (BUS,">".$busfilename)
+			or die ("Unable to open file for writing");
+		  print (BUS $firstline);
+		  print (BUS $line);
+		  close (BUS);
+		}
 
-	print $epoch."\t".$useremail.
-		"\t".$admin_role."\t".$appcount.
-		"\t".$allocated_mb."\t".$usage_mb.
-		"\t".$groups."\n";
+		close (ASPL);
+	}
 }
