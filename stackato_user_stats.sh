a1912cf71432d7207a878bc95a3e5d561a45f268
ramkumardevanathan <ramd@hp.com>
2012-12-04 04:13:11 -0800

    usernames.

--- stackato_user_stats.sh
+++ stackato_user_stats.sh
@@ -6,10 +6,10 @@
 # role.useremail.bus
 perl users.pl | awk 'BEGIN{heading="";}{
 	if (NR == 1) { heading=$0; next; }
-	if ($3 == "true") { print heading > "USER$!admin."$2".bus";
-			print $0 >> "USER$!admin."$2".bus"; }
-	if ($3 == "false") { print heading > "USER$!user."$2".bus";
-			print $0 >> "USER$!user."$2".bus"; }
+	if ($3 == "true") { print heading > "USER$!"$2".bus";
+			print $0 >> "USER$!"$2".bus"; }
+	if ($3 == "false") { print heading > "USER$!"$2".bus";
+			print $0 >> "USER$!"$2".bus"; }
 }'
 
 # create app bus files
