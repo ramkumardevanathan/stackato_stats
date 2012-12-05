2042aa1cea8c2635196103e2a1b27f2a0ed0388b
ramkumardevanathan <ramd@hp.com>
2012-12-05 02:37:34 -0800

    .

--- fns.pm
+++ fns.pm
@@ -24,11 +24,11 @@ $VERSION     = 0.01;
 sub roundn10 {
         my $n = shift;
 
-        if (($n % 10) ge 5) {
-                return $n + 10 - ($n % 10);
-        } else {
-                return $n - ($n % 10);
-        }
+        # if (($n % 10) ge 5) {
+                # return $n + 10 - ($n % 10);
+        # } else {
+                return $n - ($n % 30);
+        # }
 }
 
 sub decode {
